dd64bf247caae737e3cd111452ee8c1b1faaa259
Justin Ellis <dotjus@gmail.com>
2021-01-29 13:55:57 +1000

    Removed all uses of the Link keyword to continue troubleshooting of Heroku deploy.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Fragment } from 'react'
-import { BrowserRouter as Router, Route} from "react-router-dom";
 
 import Navbar from './components/layout/Navbar'
 import Landing from './components/layout/Landing'
@@ -7,12 +6,12 @@ import Landing from './components/layout/Landing'
 
 const App = () => {
   return (
-  <Router>
+  
     <Fragment>
       <Navbar />
       <Landing />
     </Fragment>
-  </Router>
+  
   )
 }
 
--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,26 +1,10 @@
 import React, { Component, Fragment } from 'react'
-import {
-    BrowserRouter as Router,
-    Switch,
-    Route,
-    Link
-  } from "react-router-dom"
 
 const Navbar = () => {
     return (
         <Fragment>
-            dick
+            Testing componant
         </Fragment>
-        // <Router>
-        //     <nav className="navbar navbar-dark bg-dark navbar-expand-lg">
-        //         <Link to="/" className="navbar-brand">LOGO</Link> &nbsp; - &nbsp;
-        //         Current Page &nbsp; - &nbsp;
-        //         <Link to="/" className="nav-link">Search Climbs</Link> &nbsp; - &nbsp;
-        //         <Link to="/" className="nav-link">Setting Schedule</Link> &nbsp; - &nbsp;
-        //         <Link to="/" className="nav-link">Staff Login</Link> &nbsp; - &nbsp;
-        //         <Link to="/" className="nav-link">Help</Link> 
-        //     </nav>  
-        // </Router> 
     )
 }
 
