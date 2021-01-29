21b6c987e6e281249b941634b89f8f4288d9b3ff
Justin Ellis <dotjus@gmail.com>
2021-01-29 14:12:53 +1000

    Added dummy text to Navbar component to test new github remote repository named justin.

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -3,7 +3,7 @@ import React, { Component, Fragment } from 'react'
 const Navbar = () => {
     return (
         <Fragment>
-            Testing componant
+            Testing Components!!
         </Fragment>
     )
 }
