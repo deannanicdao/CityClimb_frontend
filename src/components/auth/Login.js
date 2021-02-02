d2ea4fe04f94cb382f2c6f3f7859eb15d083dcfd
deannanicdao <deanna.nicdao@gmail.com>
2021-02-02 11:25:57 +1000

    routing, admin and auth folders in components added

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,5 +1,5 @@
 import React, { Fragment, useState } from 'react'
-import axios from 'axios'
+
 
 const Login = () => {
     const [formData, setFormData] = useState({
--- src/components/auth/Register.js
+++ src/components/auth/Register.js
@@ -1,10 +1,91 @@
-import React from 'react'
+import React, { Fragment, useState } from 'react'
+import { useForm } from 'react-hook-form'
+
 
 const Register = () => {
+    const { register, handleSubmit, errors } = useForm()
+
+    const onSubmit = (data) => {
+        const formData = new FormData()
+
+        // formData.append("name", data.name)
+        formData.append("email", data.email)
+        formData.append("staffNumber", data.staffNumber)
+        formData.append("password", data.password)
+        // formData.append("password2", data.password2)
+        formData.append("image", data.image[0])
+
+        console.log(formData)
+
+        fetch('http://localhost:8000/users', {
+            method: 'POST',
+            body: formData,
+            // headers: {
+
+            // }
+        })
+    }
+
+    console.log(errors)
+    
+    // const [formData, setFormData] = useState({
+    //     name: '',
+    //     email: '',
+    //     staffNumber: '',
+    //     password: '',
+    //     password2: ''
+    // })
+
+    // const { email, staffNumber, password } = formData
+
+    // const onChange = e => 
+    //     setFormData({ ...formData, [e.target.name]: e.target.value })
+
+    // const onSubmit = async e => {
+    //     e.preventDefault()
+    //     console.log(formData)
+
+    //     const loggedInUser = {
+    //         email, 
+    //         staffNumber,
+    //         password
+    //     }
+
+    //     try {
+    //         const config = {
+    //             headers: {
+    //                 'Content-Type': 'application/json'
+    //             }
+    //         }
+
+    //         const body = JSON.stringify(loggedInUser)
+
+    //         // const res = await axios.get('/users', body, config)
+    //         // console.log(res.data)
+    //     } catch (err) {
+    //         console.error(err.message)
+    //     }
+
+
     return (
-        <div>
-            Register
-        </div>
+        <Fragment>
+            <h1>Register</h1>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <div className='form-group'>
+                    <input type='text' ref={register({ required: true })} placeholder='Email Address' name='email' />
+                </div>
+                <div className='form-group'>
+                    <input type='text' ref={register({ required: true })} placeholder='Staff Number' name='staffNumber' />
+                </div>
+                <div className='form-group'>
+                    <input type='text' ref={register({ required: true })} placeholder='Password' name='password' />
+                </div>
+
+                <input ref={register({ required: true })} type="file" name="image" />
+
+                <input type="submit" className="btn btn-primary" value="Register"/>
+            </form>
+        </Fragment>
     )
 }
 
