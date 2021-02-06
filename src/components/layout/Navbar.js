58c468a4f219a68b3d7133fd34be6cc5953b96dc
Justin Ellis <dotjus@gmail.com>
2021-02-06 13:53:02 +1000

    Removed Reacstrap menubar and added CSS based menu bar.

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,67 +1,128 @@
-import React, { useState, Fragment } from 'react';
+import React, { useState } from 'react';
 import {
     BrowserRouter as Router,
     Switch,
     Route,
     Link
   } from "react-router-dom"
-import {
-  Collapse,
-  Navbar,
-  NavbarToggler,
-  NavbarBrand,
-  Nav,
-  NavItem,
-  NavLink
-} from 'reactstrap';
+
+import { Button } from './Button'
 
 
 const Example = (props) => {
-  const [isOpen, setIsOpen] = useState(false);
 
-  const toggle = () => setIsOpen(!isOpen);
+  // Button logic
+  const [click, setClick] = useState(false)
+  const [button, setButton] = useState(true)
+
+  // Burger Menu logic
+  const handleClick = () => setClick(!click)
+  const closeMobileMenu = () => setClick(false)
+
+  // Hides Buttons when window smaller than 960px
+  const showButton = () => {
+    if (window.innerWidth <= 960) {
+      setButton(false);
+    } else {
+      setButton(true);
+    }
+  };
+
+  useEffect(() => {
+    showButton();
+  }, []);
+
+  window.addEventListener('resize', showButton);
 
   return (
-    <div >
-      <Navbar color="light" light expand="lg" className="text-right" style={{justify_content: "flex-end"}}>
+    < >
+      {/* Enitre navbar */}
+      <nav className="navbar">
+        <div className="navbar-container">
+
+            {/* Logo */}
+            <Link to="/" className="navbar-logo">
+              <img src="./icon.png" alt="logo" width="50px" height="50px"></img>
+            </Link>
+
+            {/* Hamburger menu */}
+            {/* Sets which icon is displaying on click */}
+            <div className="menu-icon" onClick={handleClick}>
+              <i className={click ? "fas fa-times" : "fas fa-bars"} />
+            </div>
+            
+            {/* Sets the burger menu overlay on click */}
+            <ul className={ click ? "nav-menu active" : "nav-menu" } > 
+
+              {/* Individual Navigation Items */}
+              <li className="nav-item">
+                <Link to="/" className="nav-links" onClick={closeMobileMenu}>
+                  Home
+                </Link>
+              </li>
+
+              <li className="nav-item">
+                <Link to="/search" className="nav-links" onClick={closeMobileMenu}>
+                  Search
+                </Link>
+              </li>
+
+              <li className="nav-item">
+                <Link to="/schedule" className="nav-links" onClick={closeMobileMenu}>
+                  Schedule
+                </Link>
+              </li>
+
+              <li className="nav-item">
+                <Link to="/help" className="nav-links" onClick={closeMobileMenu}>
+                  Help
+                </Link>
+              </li>
+
+              <li className="nav-item">
+                <Link to="/create_climb" className="nav-links" onClick={closeMobileMenu}>
+                  Create Climb
+                </Link>
+              </li>
+
+              <li className="nav-item">
+                <Link to="/staff_login" className="nav-links-mobile" onClick={closeMobileMenu}>
+                  Staff Login
+                </Link>
+              </li>
+
+              <li className="nav-item">
+                <Link to="/staff_register" className="nav-links-mobile" onClick={closeMobileMenu}>
+                  Staff Register
+                </Link>
+              </li>
+
+              <li className="nav-item">
+                <Link to="/staff_dashboard" className="nav-links-mobile" onClick={closeMobileMenu}>
+                  Staff Dashboard
+                </Link>
+              </li>
+
+              <li className="nav-item">
+                <Link to="/climb_template" className="nav-links" onClick={closeMobileMenu}>
+                  Climb Template
+                </Link>
+              </li>
+
+            </ul> 
+
+            {button && <Button buttonStle='btn--outline'>Sign Up</Button>}
+
+        </div>
+      </nav>
+
+    </>
+
+      /* <Navbar color="light" light expand="lg" className="text-right" style={{justify_content: "flex-end"}}>
         <NavbarBrand href="/"><img src="./icon.png" alt="logo" width="50px" height="50px"></img></NavbarBrand>
         <NavbarToggler onClick={toggle} />
         <Collapse isOpen={isOpen} navbar >
           <Nav navbar >
-
-            <NavItem>
-              <NavLink> <Link to="/"> Home </Link> </NavLink>
-            </NavItem>
-
-            <NavItem>
-              <NavLink> <Link to="/search"> Search </Link> </NavLink>
-            </NavItem>
-
-            <NavItem>
-              <NavLink> <Link to="/schedule"> Schedule </Link> </NavLink>
-            </NavItem>
-
-            <NavItem>
-              <NavLink> <Link to="/help"> Help </Link> </NavLink>
-            </NavItem>
-
-            {/* Only visible when logged in as staff */}
-            <NavItem>
-              <NavLink> <Link to="/create_climb"> Create Climb </Link> </NavLink>
-            </NavItem>
-
-            {/* Only visible when no staff logged in */}
-            <NavItem>
-              <NavLink> <Link to="/staff_login"> Staff Login </Link> </NavLink>
-            </NavItem>
-
-            <NavItem>
-              <NavLink> <Link to="/staff_register"> Staff Register </Link> </NavLink>
-            </NavItem>
-
-            <NavItem>
-              <NavLink> <Link to="/staff_dashboard"> Staff Dashboard </Link> </NavLink>
-            </NavItem>
 
             <NavItem>
               <NavLink> <Link to="/climb_template"> Climb </Link> </NavLink>
@@ -69,8 +130,8 @@ const Example = (props) => {
 
             </Nav>
             </Collapse>
-          </Navbar>
-    </div>
+          </Navbar> */
+    
   )
 }
 export default Example;
