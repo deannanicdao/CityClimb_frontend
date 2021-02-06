17bc6dbfda261a5ba7b50e208d60a9b69d986f49
deannanicdao <deanna.nicdao@gmail.com>
2021-02-07 01:23:36 +1000

    Alerts are now working with redux

--- src/App.js
+++ src/App.js
@@ -1,21 +1,46 @@
-import React from 'react'
+import React, { useEffect } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navbar from './components/layout/Navbar'
 import Landing from './components/layout/Landing'
 import Routes from './components/routing/Routes'
+import { LOGOUT } from './actions/types';
+import Alert from './components/layout/Alert'
+
+// Connext redux and react
+import { Provider } from 'react-redux'
+
+// Redux
+import { loadUser } from './actions/auth';
+import store from './store'
+import setAuthToken from './utils/setAuthToken';
 
 
 const App = () => {
+  useEffect(() => {
+    // check for token in LS
+    if (localStorage.token) {
+      setAuthToken(localStorage.token);
+    }
+    store.dispatch(loadUser());
+
+    // log user out from all tabs if they log out in one tab
+    window.addEventListener('storage', () => {
+      if (!localStorage.token) store.dispatch({ type: LOGOUT });
+    });
+  }, []);
 
   return (
     <>
-      <Router>
-        <Navbar/>
-            <Switch>
-              <Route exact path="/" component={Landing} />
-              <Route component={Routes} />
-            </Switch>
-      </Router>
+      <Provider store={store}>
+        <Router>
+          <Navbar/>
+                <Alert />
+              <Switch>
+                <Route exact path="/" component={Landing} />
+                <Route component={Routes} />
+              </Switch>
+        </Router>
+      </Provider>
     </>
   )
 }
--- src/components/auth/Register.js
+++ src/components/auth/Register.js
@@ -1,94 +1,202 @@
-import React, { Fragment, useState, useRef } from 'react'
-import { useForm } from 'react-hook-form'
 
+import React, { Fragment, useState } from 'react';
+import { connect } from 'react-redux';
+import { Link, Redirect } from 'react-router-dom';
+import { setAlert } from '../../actions/alert';
+import { register } from '../../actions/auth';
+import PropTypes from 'prop-types';
 
-const Register = () => {
-    const { register, handleSubmit, errors, watch } = useForm()
-    const password = useRef({})
-    password.current = watch("password", "")
-    
-    const onSubmit = (data) => {
+const Register = ({ setAlert, register, isAuthenticated }) => {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    password: '',
+    confirmPassword: ''
+  });
 
-        console.log(data)
+  const { name, email, password, confirmPassword } = formData;
 
-        const formData = new FormData()
-        const name = `${data.firstName} ${data.lastName}`
+  const onChange = (e) =>
+    setFormData({ ...formData, [e.target.name]: e.target.value });
 
-        formData.append("name", name)
-        formData.append("email", data.email)
-        formData.append("staffNumber", data.staffNumber)
-        formData.append("password", data.password)
-        formData.append("image", data.image[0])
-
-        console.log(formData)
-
-        fetch('http://localhost:8000/users', {
-            method: 'POST',
-            body: formData,
-            // headers: {
-
-            // }
-        })
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    if (password !== confirmPassword) {
+      setAlert('Passwords do not match', 'danger');
+    } else {
+      register({ name, email, password });
     }
+  };
+
+  if (isAuthenticated) {
+    return <Redirect to="/dashboard" />;
+  }
+
+  return (
+    <Fragment>
+      <h1 className="large text-primary">Sign Up</h1>
+      <p className="lead">
+        <i className="fas fa-user" /> Create Your Account
+      </p>
+      <form className="form" onSubmit={onSubmit}>
+        <div className="form-group">
+          <input
+            type="text"
+            placeholder="Name"
+            name="name"
+            value={name}
+            onChange={onChange}
+          />
+        </div>
+        <div className="form-group">
+          <input
+            type="email"
+            placeholder="Email Address"
+            name="email"
+            value={email}
+            onChange={onChange}
+          />
+          <small className="form-text">
+            Profile image provided via Gravatar
+          </small>
+        </div>
+        <div className="form-group">
+          <input
+            type="password"
+            placeholder="Password"
+            name="password"
+            value={password}
+            onChange={onChange}
+          />
+        </div>
+        <div className="form-group">
+          <input
+            type="password"
+            placeholder="Confirm Password"
+            name="confirmPassword"
+            value={confirmPassword}
+            onChange={onChange}
+          />
+        </div>
+        <input type="submit" className="btn btn-primary" value="Register" />
+      </form>
+      <p className="my-1">
+        Already have an account? <Link to="/login">Sign In</Link>
+      </p>
+    </Fragment>
+  );
+};
+
+Register.propTypes = {
+  setAlert: PropTypes.func.isRequired,
+  register: PropTypes.func.isRequired,
+  isAuthenticated: PropTypes.bool
+};
+
+const mapStateToProps = (state) => ({
+  isAuthenticated: state.auth.isAuthenticated
+});
+
+export default connect(mapStateToProps, { setAlert, register })(Register);
+// import React, { Fragment, useState, useRef } from 'react'
+// import { connect } from 'react-redux'
+// import { useForm } from 'react-hook-form'
+// import { setAlert } from '../../actions/alert'
+
+
+// const Register = (props) => {
+//     const { register, handleSubmit, errors, watch } = useForm()
+//     const password = useRef({})
+//     password.current = watch("password", "")
+    
+//     const onSubmit = (data) => {
+//         e.preventDefault();
+//         if (password !== confirmPassword) {
+//           setAlert('Passwords do not match', 'danger');
+//         } else {
+//           const formData = new FormData()
+//           const name = `${data.firstName} ${data.lastName}`
+  
+//           formData.append("name", name)
+//           formData.append("email", data.email)
+//           formData.append("staffNumber", data.staffNumber)
+//           formData.append("password", data.password)
+//           formData.append("image", data.image[0])
+  
+//           console.log(formData)
+  
+//           fetch('http://localhost:8000/users', {
+//               method: 'POST',
+//               body: formData,
+//               // headers: {
+  
+//               // }
+//           })
+//         }
+
+//         console.log(data)
+//         props.setAlert(`This is the data: ${data}`)
+
+//     }
     
 
 
-    return (
-        <Fragment>
-            <h1>Register</h1>
-            <form onSubmit={handleSubmit(onSubmit)}>
-                <div className='form-group'>
-                    <input type='text' ref={register({ required: true })} placeholder='First Name' name='firstName' />
-                </div>
-                <div className='form-group'>
-                    <input type='text' ref={register({ required: true })} placeholder='Last Name' name='lastName' />
-                </div>
-                <div className='form-group'>
-                    <input type='text' ref={register({ required: true })} placeholder='Email Address' name='email' />
-                </div>
-                <div className='form-group'>
-                    <input type='text' ref={register({ required: true })} placeholder='Staff Number' name='staffNumber' />
-                </div>
-                <div className='form-group'>
-                    {/* <input type='password' ref={register({ required: true })} placeholder='Password' name='password' /> */}
-                    <input
-                        type="password"
-                        name="password"
-                        placeholder="Password"
-                        ref={register({
-                        required: "You must specify a password",
-                        minLength: {
-                            value: 8,
-                            message: "Password must have at least 8 characters"
-                        }
-                        })}
-                    />
-                </div>
-                {errors.password && <p>{errors.password.message}</p>}
-                <div className='form-group'>
-                    {/* <input type='password' placeholder='Confirm Password' name='confirmPassword' ref={register({
-                        required: true,
-                        validate: value => 
-                            value === password.current || "Passwords must match"
-                    })} /> */}
-                    <input
-                        name="confirmPassword"
-                        type="password"
-                        placeholder="Confirm password"
-                        // ref={register({
-                        // validate: value =>
-                        //     value === password.current || "The passwords do not match"
-                        // })}
-                    />
-                </div>
-                {/* {errors.confirmPassword && <p>{errors.confirmPassword.message}</p>} */}
+//     return (
+//         <Fragment>
+//             <h1>Register</h1>
+//             <form onSubmit={handleSubmit(onSubmit)}>
+//                 <div className='form-group'>
+//                     <input type='text' ref={register({ required: true })} placeholder='First Name' name='firstName' />
+//                 </div>
+//                 <div className='form-group'>
+//                     <input type='text' ref={register({ required: true })} placeholder='Last Name' name='lastName' />
+//                 </div>
+//                 <div className='form-group'>
+//                     <input type='text' ref={register({ required: true })} placeholder='Email Address' name='email' />
+//                 </div>
+//                 <div className='form-group'>
+//                     <input type='text' ref={register({ required: true })} placeholder='Staff Number' name='staffNumber' />
+//                 </div>
+//                 <div className='form-group'>
+//                     {/* <input type='password' ref={register({ required: true })} placeholder='Password' name='password' /> */}
+//                     <input
+//                         type="password"
+//                         name="password"
+//                         placeholder="Password"
+//                         ref={register({
+//                         required: "You must specify a password",
+//                         minLength: {
+//                             value: 8,
+//                             message: "Password must have at least 8 characters"
+//                         }
+//                         })}
+//                     />
+//                 </div>
+//                 {errors.password && <p>{errors.password.message}</p>}
+//                 <div className='form-group'>
+//                     {/* <input type='password' placeholder='Confirm Password' name='confirmPassword' ref={register({
+//                         required: true,
+//                         validate: value => 
+//                             value === password.current || "Passwords must match"
+//                     })} /> */}
+//                     <input
+//                         name="confirmPassword"
+//                         type="password"
+//                         placeholder="Confirm password"
+//                         // ref={register({
+//                         // validate: value =>
+//                         //     value === password.current || "The passwords do not match"
+//                         // })}
+//                     />
+//                 </div>
+//                 {/* {errors.confirmPassword && <p>{errors.confirmPassword.message}</p>} */}
 
-                <input ref={register} type="file" name="image" />
+//                 <input ref={register} type="file" name="image" />
 
-                <input type="submit" className="btn btn-primary" value="Register"/>
-            </form>
-        </Fragment>
-    )
-}
+//                 <input type="submit" className="btn btn-primary" value="Register"/>
+//             </form>
+//         </Fragment>
+//     )
+// }
 
-export default Register
+// export default connect(null, { setAlert })(Register) // connect takes in state, actions - this allows us to send through props
