9c0def6dc1406a5ce35622a9d313c4125898150d
deannanicdao <deanna.nicdao@gmail.com>
2021-02-07 13:53:11 +1000

    Initial data fetch of all users working

--- src/components/admin/StaffDashboard.js
+++ src/components/admin/StaffDashboard.js
@@ -1,9 +1,28 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+// import { Row, Col } from 'react-bootstrap';
 
 const StaffDashboard = () => {
+    const [users, setUsers] = useState([])
+
+    useEffect(() => {
+
+        fetch("http://localhost:8000/users")
+        .then(res => res.json())
+        .then(data => { 
+            console.log(data)
+            setUsers(data)
+        })
+        .catch(e => console.log(e))
+       
+        return () => {
+            console.log('Unmounted (inside schedule)')
+        }
+    }, [])
+
     return (
         <div>
             Dashboard for Staff
+            <p>Users: {(users).map(user => user.name)}</p>
         </div>
     )
 }
