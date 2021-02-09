185532cadd5daa443471db46f1c8ff902a337bce
Jeremy Bell <jeremy.bell91@gmail.com>
2021-02-09 11:11:12 +1000

    Form validation for edit climb form, changed wall to title

--- src/components/climbs/forms/CreateClimbForm.js
+++ src/components/climbs/forms/CreateClimbForm.js
@@ -15,7 +15,7 @@ const CreateClimbForm = () => {
         const formData = new FormData()
 
         formData.append("gym", data.gym)
-        formData.append("wall", data.wall)
+        formData.append("title", data.title)
         formData.append("colour", data.colour)
         formData.append("youtubeUrl", data.youtubeUrl)
         formData.append("image", data.image[0])
@@ -51,7 +51,7 @@ const CreateClimbForm = () => {
 
                 <label className="text-gray-700">
                 Climb Name
-      <input type="text" name="wall" placeholder="Climb name" ref={register({ required: true })} className="block w-52 py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-primary-500 focus:border-primary-500">
+      <input type="text" name="title" placeholder="Climb name" ref={register({ required: true })} className="block w-52 py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-primary-500 focus:border-primary-500">
                     </input>
                 </label>
 
